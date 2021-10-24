7cb04ee033d76fab42e82584422d764b14d35bf9
Rinat Valiullov <4rontender@gmail.com>
2021-10-24 21:31:36 +0500

    add "SearchBar" component

--- product-table/src/components/FilterableProductTable/FilterableProductTable.jsx
+++ product-table/src/components/FilterableProductTable/FilterableProductTable.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './FilterableProductTable.module.css';
 import { ProductTable } from '../ProductTable/ProductTable';
+import { SearchBar } from '../SearchBar/SearchBar';
 
 const FilterableProductTable = ({ products }) => {
   return (
     <div className={classes.container}>
+      <SearchBar />
       <ProductTable products={products} />
     </div>
   );
--- product-table/src/components/SearchBar/SearchBar.jsx
+++ product-table/src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
-import { classes } from './SearchBar.module.css';
+import classes from './SearchBar.module.css';
 
-const SearchBar = () => { };
+const SearchBar = () => {
+  return (
+    <div className={classes.searchContainer}>
+      <form>
+        <input type="text" placeholder="Search..." className={classes.searchInput} />
+        <div className={classes.searchCheckContainer}>
+          <input type="checkbox" id="searchCheck" />
+          <label htmlFor="searchCheck">Show product that stocked</label>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export { SearchBar };

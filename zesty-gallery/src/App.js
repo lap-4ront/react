0b3891fd66707aa6b36ec1effd1a539abe636e57
Rinat Valiullov <4rontender@gmail.com>
2022-02-20 23:49:54 +0500

    update the index of image position with a click handler

--- zesty-gallery/src/App.js
+++ zesty-gallery/src/App.js
@@ -1,18 +1,37 @@
 import "./App.css";
+import React, { useState } from "react";
+import tabitha from "./assets/image1.jpg";
+import ardy from "./assets/image2.jpg";
+import elise from "./assets/image3.jpg";
+import mahdi from "./assets/image4.jpg";
+import osarugue from "./assets/image5.jpg";
 
-const App = () => (
-  <div className="App">
-    <div className="title">
-      <h1>Zesty</h1>
-      <h2>
-        A photo gallery app <br /> by 4rontender
-      </h2>
-    </div>
+const images = [tabitha, ardy, elise, mahdi, osarugue];
 
-    <div className="image-container">
-      <img alt="" src="" />
+const App = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const handleClick = () => {
+    const length = images.length - 1;
+    setCurrentImage((currentImage) => {
+      return currentImage < length ? currentImage + 1 : 0;
+    });
+  };
+
+  return (
+    <div className="App">
+      <div className="title">
+        <h1>Zesty</h1>
+        <h2>
+          A photogallery app <br /> by 4rontender
+        </h2>
+      </div>
+
+      <div className="image-container">
+        <img alt="" src={images[currentImage]} onClick={handleClick} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;

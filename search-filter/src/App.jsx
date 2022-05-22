6c9d481a1767b5b7210687cc8b3b5bdcff16bab7
Rinat Valiullov <4rontender@gmail.com>
2022-05-23 03:14:15 +0500

    refactored search function

--- search-filter/src/App.jsx
+++ search-filter/src/App.jsx
@@ -6,12 +6,11 @@ import { Users } from "./users";
 function App() {
   const [query, setQuery] = useState("");
 
+  const keys = ["first_name", "last_name", "email"];
+
   const search = (data) => {
-    return data.filter(
-      (item) =>
-        item.first_name.toLowerCase().includes(query) ||
-        item.last_name.toLowerCase().includes(query) ||
-        item.email.toLowerCase().includes(query)
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
     );
   };
 

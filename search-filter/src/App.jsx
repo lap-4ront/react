05a4f2f1ee4d3d75246db20605f817ccc8620c48
Rinat Valiullov <4rontender@gmail.com>
2022-05-23 03:06:35 +0500

    advanced search function

--- search-filter/src/App.jsx
+++ search-filter/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import { Table } from "./Table";
 import { Users } from "./users";
 
 function App() {
   const [query, setQuery] = useState("");
 
-  const filteredUsers = (users) =>
-    users.filter((user) => user.first_name.toLowerCase().includes(query));
+  const search = (data) => {
+    return data.filter(
+      (item) =>
+        item.first_name.toLowerCase().includes(query) ||
+        item.last_name.toLowerCase().includes(query) ||
+        item.email.toLowerCase().includes(query)
+    );
+  };
 
   return (
     <div className="App">
@@ -16,13 +23,7 @@ function App() {
         className="search"
         onChange={(evt) => setQuery(evt.target.value)}
       />
-      <ul className="list">
-        {filteredUsers(Users).map((user) => (
-          <li className="listItem" key={user.id}>
-            {user.first_name}
-          </li>
-        ))}
-      </ul>
+      <Table data={search(Users)} />
     </div>
   );
 }

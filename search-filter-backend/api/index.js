c663b38a131ae190cdf78335306d463eab2829c4
Rinat Valiullov <4rontender@gmail.com>
2022-05-23 17:03:19 +0500

    search for a specific query

--- search-filter-backend/api/index.js
+++ search-filter-backend/api/index.js
@@ -7,7 +7,17 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json(Users);
+  const { q } = req.query;
+
+  const keys = ["first_name", "last_name", "email"];
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(q))
+    );
+  };
+
+  res.json(search(Users).splice(0, 10));
 });
 
 app.listen(5000, console.log("Server is running..."));

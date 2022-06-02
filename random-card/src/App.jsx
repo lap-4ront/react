9248c3d38c4c2b528916d28e7dcae78cfdc44609
Rinat Valiullov <4rontender@gmail.com>
2022-06-02 23:44:30 +0500

    change styles

--- random-card/src/App.jsx
+++ random-card/src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
+import styled from "styled-components";
 import { Card } from "./Card";
 import { getCard } from "./cards";
+
+const SECTION = styled.section`
+  padding: 2rem;
+  text-align: center;
+`;
 
 function App() {
   const [card, setCard] = useState(getCard());
@@ -11,10 +17,10 @@ function App() {
   };
 
   return (
-    <>
+    <SECTION>
       <button onClick={setRandomCard}>Get new card</button>
       <Card value={card}></Card>
-    </>
+    </SECTION>
   );
 }
 
--- random-card/src/index.js
+++ random-card/src/index.js
@@ -1,11 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
 import App from "./App";
-import "./index.css";
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 24px;
+  }
+
+  body {
+    background-color: #cecece;
+    margin: auto;
+  }
+
+  button {
+    background: #3a31d1;
+    box-shadow: 2px 2px 4px 0px #621c5a;
+    border: none;
+    border-radius: 5px;
+    color: #42d9fb;
+    cursor: pointer;
+    padding: 10px 20px;
+  }
+
+  button:active {
+    box-shadow: none;
+    transform: translate(2px, 2px);
+  }
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
